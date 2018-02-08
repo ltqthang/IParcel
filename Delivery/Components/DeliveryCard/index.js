d313282fb32bdf9bba4bd7a251c77bfb86c01672
Thang <letqt@garena.com>
2018-02-08 15:05:45 +0800

    Initial React Native app

---
+++ Delivery/Components/DeliveryCard/index.js
@@ -0,0 +1,21 @@
+import {TouchableHighlight, View, Image, StyleSheet, Text} from 'react-native';
+import React, {Component, PropTypes} from 'react';
+
+const DeliveryCard = ({}: {}, {rootTag}) => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  icon: {},
+  text: {
+    ...R14,
+  }
+});
+
+
+export default DeliveryCard
