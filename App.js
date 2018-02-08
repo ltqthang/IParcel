e2925e5403f65ca891bed827b47e36395f47398d
Thang <letqt@garena.com>
2018-02-08 15:55:24 +0800

    Add delivery list UI and mock data

--- App.js
+++ App.js
@@ -9,16 +9,32 @@ import {
   FlatList,
   StyleSheet,
   Text,
-  View
+  View,
+  Platform
 } from 'react-native';
+import mockdata from "./utils/mockdata";
+import DeliveryCard from "./Delivery/Components/DeliveryCard";
 
 type Props = {};
+
+const deliveries = mockdata;
+
 export default class App extends Component<Props> {
+
+  renderDeliveryCard({item}) {
+    return (
+      <DeliveryCard
+        delivery={item}/>
+    )
+  }
 
   render() {
     return (
       <View style={styles.container}>
-
+        <FlatList
+          data={deliveries}
+          renderItem={this.renderDeliveryCard}
+        />
       </View>
     );
   }
@@ -27,6 +43,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 36 : 0,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
--- Delivery/Components/DeliveryCard/index.js
+++ Delivery/Components/DeliveryCard/index.js
@@ -1,20 +1,38 @@
-import {TouchableHighlight, View, Image, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, Dimensions} from 'react-native';
 import React, {Component, PropTypes} from 'react';
+import {Card, Button} from 'react-native-elements'
+const WINDOW_WIDTH = Dimensions.get('window').width;
 
-const DeliveryCard = ({}: {}, {rootTag}) => {
+const DeliveryCard = ({delivery}: {}) => {
   return (
-    <View style={styles.container}>
+    <Card
+      containerStyle={{width: WINDOW_WIDTH - 32}}
+      title={delivery.customer}
+      image={{uri: delivery.image}}>
 
-    </View>
+      <Text>
+        <Text h4 style={{fontWeight: 'bold'}}>{'From: '}</Text>
+        <Text h4>{delivery.from.name}</Text>
+      </Text>
+      <Text>
+        <Text style={{fontWeight: 'bold'}}>{'From: '}</Text>
+        <Text h4>{delivery.to.name}</Text>
+      </Text>
+      <View style={{height: 16}}/>
+
+      <Button
+        backgroundColor='#03A9F4'
+        fontFamily='Lato'
+        buttonStyle={{borderRadius: 2}}
+        text='Pick Up Now'/>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
   icon: {},
-  text: {
-    ...R14,
-  }
+  text: {}
 });
 
 
